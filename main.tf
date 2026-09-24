582059857c3882adbbd27750f81ee9c71d86c83e
Manasa ramya <manasaramyagaddamadugu@gmail.com>
2026-09-24 17:10:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "foo" {
 
 # S3 Bucket
 resource "aws_s3_bucket" "demo" {
-  bucket = "testhydbucket20260910"
+  bucket = "test-lambda-jenkins-bucket-2007"
 
   tags = {
     Name        = "Terraform-S3"
